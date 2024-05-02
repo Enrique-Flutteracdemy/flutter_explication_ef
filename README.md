0ddfc35b1c81cef80bf4be543568382e2b7ff97c
JoseManuelBarba <josemanuelbarbasanchez@gmail.com>
2024-05-02 20:15:36 +0200

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_explication_ef
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_explication_ef/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_explication_ef
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
